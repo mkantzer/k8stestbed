5d9f811d367295e3a7e1dcc8bf985f75d0a02817
Mike Kantzer <mbkantzer@gmail.com>
2017-12-10 10:22:32 -0500

    First successful thing

---
+++ src/helloworld.go
@@ -0,0 +1,11 @@
+// starting out with a small program that just prints
+// to the command line. This will eventually be extended into
+// a webserver microservice to serve along network
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world!")
+}
