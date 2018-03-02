31b82a7f803c9d3532455614c5331e3fe5158449
Mike Kantzer <mbkantzer@gmail.com>
2018-03-02 15:33:24 -0500

    broke everythingm, but thats okay

--- cmd/helloworld/main.go
+++ cmd/helloworld/main.go
@@ -1,10 +1,15 @@
 // starting out with a small program that just prints
 // to the command line, as proof of repo structure
 
+//I'm pretty sure I've broken this all, because of a missunderstanding of how structs and packages works.
+// I may need to work on a simpler data project to figure this stuff out
+
 package main
 
 import (
 	"fmt"
+
+	spew "github.com/davecgh/go-spew/spew"
 
 	players "github.com/mkantzer/k8stestbed/pkg/players"
 )
@@ -12,4 +17,8 @@ import (
 func main() {
 	cal := players.Player{Name: "Cal Ripken Jr"}
 	fmt.Println(cal.Name)
+	spew.Dump(cal)
+	g1 := players.Game{gameNumber: 1, opponent: "Yankies", rbi: 5}
+	g2 := players.Game{gameNumber: 2, opponent: "Seattle", rbi: 1}
+	s := players.Season{season}
 }
--- pkg/players/players.go
+++ pkg/players/players.go
@@ -2,21 +2,41 @@
 
 package players
 
-//Player is a structure that includes a name and a slice of struct Seasons
+//Player is a structure that includes a name and a slice of struct seasons
 type Player struct {
-	Name string
-	season
+	Name    string
+	Seasons []Season
 }
 
-type season struct {
+//Season describes a full season worth of games, and a year the season took place durring
+type Season struct {
 	seasonYear int
-	games      []struct {
-		gameNumber int
-		opponent   string
-		rbi        int
+	Games      []Game
+}
+
+//Game describes a game for a player: what game number of the season it was, what opponent they were playing, and how many rbis they hit
+type Game struct {
+	gameNumber int
+	opponent   string
+	rbi        int
+}
+
+//AppendSeasons appends a number of seasons to a player
+func (p *Player) AppendSeasons(s ...Season) {
+	//What happens if no seasons passed in?
+	//Should error if a season is repeated
+	//Should we be able to append games to an already created season?
+	for _, season := range s {
+		p.Seasons = append(p.Seasons, season)
 	}
 }
 
-func (p *Player) appendSeason(s season) {
-
+//AppendGames appends a number of games to a season
+func (s *Season) AppendGames(g ...Game) {
+	//What happens if no games passed in?
+	//Should also error if we repeat a game number
+	//Should we be able to append games to an already created season?
+	for _, game := range g {
+		s.Games = append(s.Games, game)
+	}
 }
